9e27fcc3ababd30b2fdfce7a23fcf436bbb1bd72
Your Name <you@example.com>
2026-05-22 15:48:24 +0700

    elo

--- database.sql
+++ database.sql
@@ -8,6 +8,31 @@ CREATE TABLE users (
     ROLE ENUM('admin', 'user') DEFAULT 'user'
 );
 
+CREATE TABLE resep (
+    id_resep INT AUTO_INCREMENT PRIMARY KEY, 
+    id INT NOT NULL,                          
+    judul VARCHAR(255) NOT NULL,
+    deskripsi TEXT,
+    kategori_daerah ENUM('jawa', 'kaltim', 'sumatra', 'papua', 'lainnya') NOT NULL,
+    foto VARCHAR(255) NOT NULL,
+    bahan TEXT NOT NULL,
+    langkah TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE komentar (
+    id_komentar INT AUTO_INCREMENT PRIMARY KEY,
+    id_resep INT NOT NULL,
+    id INT NOT NULL,
+    isi_komentar TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (id_resep) REFERENCES resep(id_resep) ON DELETE CASCADE,
+    FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE
+);
+
 -- username : admin 
 -- pw : admin123
 
